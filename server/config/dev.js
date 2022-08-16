0edc36304afae94fe3992587b1ab06e082d3502a
Sangati Sharan <88509488+SanSharan02@users.noreply.github.com>
2022-08-17 00:24:21 +0530

    Add files via upload

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports={
+    MONGOURI:"mongodb://SanSharan20:Sanchati@ac-1bbajmm-shard-00-00.gj4q69l.mongodb.net:27017,ac-1bbajmm-shard-00-01.gj4q69l.mongodb.net:27017,ac-1bbajmm-shard-00-02.gj4q69l.mongodb.net:27017/?ssl=true&replicaSet=atlas-ou645f-shard-0&authSource=admin&retryWrites=true&w=majority",
+    //MONGOURI:"mongodb+srv://SanSharan20:Sanchati@cluster0.gj4q69l.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"abcsefghij",
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
+}
